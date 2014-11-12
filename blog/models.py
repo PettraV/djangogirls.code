7108496329ffe42220cc84109a8d7d3e96b2cb18
Petra Vidmar <pettra.vidmar@gmail.com>
2014-11-12 19:36:00 +0100

    adding missing stuff - goranb

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
-from django.db.migrations import Migration
+# from django.db.migrations import Migration
 
 class Post(models.Model):
     author = models.ForeignKey(User)
